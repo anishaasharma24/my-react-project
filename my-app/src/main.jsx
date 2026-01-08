26405e46a60524131d951f7f09bba8e733746c49
anishaasharma24 <anishasharma50342@gmail.com~>
2026-01-08 19:23:38 +0530

    React project

---
+++ my-app/src/main.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import "bootstrap-icons/font/bootstrap-icons.css";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { BrowserRouter } from 'react-router-dom';
+
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
